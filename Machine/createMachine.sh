d89eb7c18bc66223137e38f0854f6d7c0e17c22d
konstruktoid <konstruktoid@users.noreply.github.com>
2015-02-22 02:52:02 +0100

    accesstoken has to exist

--- Machine/createMachine.sh
+++ Machine/createMachine.sh
@@ -6,23 +6,30 @@ MACHINE="/usr/local/bin/docker-machine"
 MACHINERELEASE="https://github.com/docker/machine/releases/download/v0.1.0-rc4/docker-machine_linux-amd64"
 ACCESSTOKEN=""
 
+if [ -z $ACCESSTOKEN ];
+        then
+                echo "ACCESSTOKEN required"
+                exit
+fi
+
 if ! test -x "$MACHINE";
-	then
-		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-		sudo apt-get update && sudo apt-get -y upgrade 
-		sudo apt-get install lxc-docker
-		sudo addgroup `whoami` docker
-		wget $MACHINERELEASE
-		sudo mv docker-machine_linux-amd64 /usr/local/bin/
-		sudo chown root:root /usr/local/bin/docker-machine_linux-amd64
-		sudo ln -vfs /usr/local/bin/docker-machine_linux-amd64 "$MACHINE"
-		docker-machine -v
+        then
+                sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+                sudo apt-get update && sudo apt-get -y upgrade
+                sudo apt-get install lxc-docker
+                sudo addgroup `whoami` docker
+                wget $MACHINERELEASE
+                sudo mv docker-machine_linux-amd64 /usr/local/bin/
+                sudo chown root:root /usr/local/bin/docker-machine_linux-amd64
+                sudo ln -vfs /usr/local/bin/docker-machine_linux-amd64 "$MACHINE"
+                docker-machine -v
 fi
 
 INPUT="$@"
 if [ -z $INPUT ];
         then
                 echo "Machine name required"
+                exit
         else
                 docker-machine create --driver digitalocean --digitalocean-access-token $ACCESSTOKEN --digitalocean-region ams2 $@
                 docker-machine ssh $@ 'source < ( https://raw.githubusercontent.com/konstruktoid/Docker/master/Security/baselineDocker.sh )'
