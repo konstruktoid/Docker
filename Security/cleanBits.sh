52c78b3c4d0705362c6290a33913530ef3ef55d0
konstruktoid <konstruktoid@users.noreply.github.com>
2015-02-22 04:17:10 +0100

    back to basics

--- Security/cleanBits.sh
+++ Security/cleanBits.sh
@@ -2,14 +2,10 @@
 
 for file in `find / -perm -02000`;
         do
-                if test -e $file;
-                        then chmod -s $file
-                fi
-        done
+			chmod -s $file
+		done
 
 for file in `find / -perm -04000`;
         do
-                if test -e $file;
-                        then chmod -s $file
-                fi
-        done
+			chmod -s $file
+		done
