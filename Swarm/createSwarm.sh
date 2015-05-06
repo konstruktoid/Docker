794d7f4b20e37897b3044353ae6062cf1999eea9
konstruktoid <konstruktoid@users.noreply.github.com>
2015-05-07 00:11:38 +0200

    retab

--- Swarm/createSwarm.sh
+++ Swarm/createSwarm.sh
@@ -11,31 +11,31 @@ ACCESSTOKEN=""
 REGION="ams3"
 
 if ! test -d "$TMP";
-	then
-		TMP=/tmp
+  then
+    TMP=/tmp
 fi
 
 if [ -z $ACCESSTOKEN ];
-	then
-		echo "ACCESSTOKEN required"
-		exit
+  then
+    echo "ACCESSTOKEN required"
+    exit
 fi
 
 if [ -z "$ORIGIN" ] || [ -z "$MASTER" ] || [ -z "$NODES" ];
-	then
-		echo "Swarm machine names need to be set up"
-		exit
-	else
-		docker-machine create --driver digitalocean --digitalocean-access-token $ACCESSTOKEN --digitalocean-region $REGION $ORIGIN
-		$(docker-machine env $ORIGIN)
+  then
+    echo "Swarm machine names need to be set up"
+    exit
+  else
+    docker-machine create --driver digitalocean --digitalocean-access-token $ACCESSTOKEN --digitalocean-region $REGION $ORIGIN
+    $(docker-machine env $ORIGIN)
 
-		docker run swarm create 1> $TMP/swarm
-		SWARMTOKEN=`cat $TMP/swarm`
+    docker run swarm create 1> $TMP/swarm
+    SWARMTOKEN=`cat $TMP/swarm`
 
-		docker-machine create --swarm --swarm-master --swarm-discovery token://`echo $SWARMTOKEN` --driver digitalocean --digitalocean-access-token $ACCESSTOKEN --digitalocean-region $REGION $MASTER 
+    docker-machine create --swarm --swarm-master --swarm-discovery token://`echo $SWARMTOKEN` --driver digitalocean --digitalocean-access-token $ACCESSTOKEN --digitalocean-region $REGION $MASTER 
 
-		for node in $NODES;
- 			do
-				docker-machine create --swarm  --swarm-discovery token://`echo $SWARMTOKEN` --driver digitalocean --digitalocean-access-token $ACCESSTOKEN --digitalocean-region $REGION $node
-			done
+    for node in $NODES;
+      do
+        docker-machine create --swarm  --swarm-discovery token://`echo $SWARMTOKEN` --driver digitalocean --digitalocean-access-token $ACCESSTOKEN --digitalocean-region $REGION $node
+      done
 fi
