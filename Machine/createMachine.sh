8d2f832fb8d53b4dacbb0d99176c2b105b682e44
konstruktoid <konstruktoid@users.noreply.github.com>
2015-05-07 00:08:04 +0200

    retab

--- Machine/createMachine.sh
+++ Machine/createMachine.sh
@@ -11,46 +11,46 @@ INPUT="$@"
 
 
 if ! test -d "$TMP";
-	then
-		TMP=/tmp
+  then
+    TMP=/tmp
 fi
 
 if [ "$INPUT" == "upgrade" ];
-	then
-		cd $TMP && wget -nv $MACHINERELEASE
-		sudo mv $TMP/docker-machine_linux-amd64 /usr/local/bin/
-		sudo chown root:root /usr/local/bin/docker-machine_linux-amd64
-		sudo chmod 0755 /usr/local/bin/docker-machine_linux-amd64
-		sudo ln -vfs /usr/local/bin/docker-machine_linux-amd64 "$MACHINE"
-		docker-machine -v
-		exit
+  then
+    cd $TMP && wget -nv $MACHINERELEASE
+    sudo mv $TMP/docker-machine_linux-amd64 /usr/local/bin/
+    sudo chown root:root /usr/local/bin/docker-machine_linux-amd64
+    sudo chmod 0755 /usr/local/bin/docker-machine_linux-amd64
+    sudo ln -vfs /usr/local/bin/docker-machine_linux-amd64 "$MACHINE"
+    docker-machine -v
+    exit
 fi
 
 if [ -z $ACCESSTOKEN ];
-	then
-		echo "ACCESSTOKEN required"
-		exit
+  then
+    echo "ACCESSTOKEN required"
+    exit
 fi
 
 if ! test -x "$MACHINE";
-	then
-		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9 
-		sudo apt-get update && sudo apt-get -y upgrade
-		sudo apt-get install lxc-docker
-		sudo addgroup `whoami` docker
-		cd $TMP && wget -nv $MACHINERELEASE
-		sudo mv $TMP/docker-machine_linux-amd64 /usr/local/bin/
-		sudo chown root:root /usr/local/bin/docker-machine_linux-amd64
-		sudo chmod 0755 /usr/local/bin/docker-machine_linux-amd64
-		sudo ln -vfs /usr/local/bin/docker-machine_linux-amd64 "$MACHINE"
-		docker-machine -v
+  then
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9 
+    sudo apt-get update && sudo apt-get -y upgrade
+    sudo apt-get install lxc-docker
+    sudo addgroup `whoami` docker
+    cd $TMP && wget -nv $MACHINERELEASE
+    sudo mv $TMP/docker-machine_linux-amd64 /usr/local/bin/
+    sudo chown root:root /usr/local/bin/docker-machine_linux-amd64
+    sudo chmod 0755 /usr/local/bin/docker-machine_linux-amd64
+    sudo ln -vfs /usr/local/bin/docker-machine_linux-amd64 "$MACHINE"
+    docker-machine -v
 fi
 
 if [ -z $INPUT ];
-	then
-		echo "Machine name required"
-		exit
-	else
-		docker-machine create --driver digitalocean --digitalocean-access-token $ACCESSTOKEN --digitalocean-region ams2 $@
-		docker-machine ssh $@ "wget -O /tmp/baselineDockerHost.sh $HARDENING; /bin/bash /tmp/baselineDockerHost.sh"
+  then
+    echo "Machine name required"
+    exit
+  else
+    docker-machine create --driver digitalocean --digitalocean-access-token $ACCESSTOKEN --digitalocean-region ams2 $@
+    docker-machine ssh $@ "wget -O /tmp/baselineDockerHost.sh $HARDENING; /bin/bash /tmp/baselineDockerHost.sh"
 fi
