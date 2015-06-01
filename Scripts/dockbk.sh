918384d11cb4c41305f25d7588940592a9580d7c
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2015-06-01 23:11:05 +0200

    add container export script

---
+++ Scripts/dockbk.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+storage='/tmp'
+log="$storage/dockbk.log"
+
+ccnt=$(echo $(docker ps -q | wc -l) | awk '{print $1}')
+ecnt=0
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "${bold}Starting export of Docker containers${normal}"
+echo "    - $ccnt running containers"
+echo "    - target directory: $storage"
+echo "    - log file: $log"
+echo
+
+if [[ -f "$log" && $(grep "Last run" "$log") ]]; then
+  sed --in-place=".bak" "s/^# Last run.*/# Last run $(date)/" "$log"
+  else
+  echo "# Last run $(date)" > "$log"
+fi
+
+cmd=$(docker ps -q)
+for b in $cmd; do
+  name=$(docker inspect -f {{.Name}} $b| tr -d '/')
+  time=$(date +%y%m%d%H%M%S)
+  filename="$name"_"$time".tar
+  ((ecnt++))
+
+  echo "[$ecnt/$ccnt] $name"
+
+  echo "    - exporting $name to $storage/$filename"
+  docker export -o $storage/$filename $b
+
+  echo "    - compressing $filename"
+  gzip "$storage/$filename"
+
+  sum=$(openssl sha1 -sha256 $storage/$filename.gz)
+  echo "$sum" >> "$log"
+  echo "    - $filename.gz checksum $(echo "$sum" | awk '{print $NF}')"
+  echo
+done
