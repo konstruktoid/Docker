808279185646e397480a2c6c8eb5a9240e971a4d
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2015-06-12 01:47:32 +0200

    autobuild if there is any new debian vuln

---
+++ Scripts/autobuild.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# https://jpetazzo.github.io/2015/05/27/docker-images-vulnerabilities/
+#
+
+BASE=~/Git
+U=$(curl -s "https://www.debian.org/security/dsa")
+UDATE=$(echo $U | grep -oP "(?<=<dc:date>)[^<]+" | head -n1)
+DEBUPD=$(date -d "$UDATE" +%s)
+
+cd "$BASE"
+for d in $(find $BASE/*_Build -name "Dockerfile"); do
+  if [ "$(grep '# Force autobuild' $d)" ]; then
+    if [ "$(grep '# Force autobuild' $d | awk '{print $NF}')" -lt "$DEBUPD" ]; then
+      echo "$d ($(grep '# Force autobuild' $d | awk '{print $NF}')) is older than $DEBUPD"
+      cd "$(echo "$d" | sed 's/Dockerfile//g')"
+      git co master
+      sed -i "s/# Force autobuild.*/# Force autobuild $(date +%s)/" $d
+      git add Dockerfile
+      git commit -m "Force autobuild"
+      git push
+      cd "$base"
+    fi
+  fi
+done
