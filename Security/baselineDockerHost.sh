0fdf2f89c53697b8dbad8a3c6607f4d83a19b735
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2015-07-01 21:18:33 +0200

    whitespaces
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- Security/baselineDockerHost.sh
+++ Security/baselineDockerHost.sh
@@ -27,14 +27,14 @@ echo root > /etc/cron.allow
 echo root > /etc/at.allow
 
 echo ":: Removing users"
-for users in games gnats irc news uucp; 
-do 
+for users in games gnats irc news uucp;
+do
 	sudo userdel -r $users 2> /dev/null
 done
 
 echo ":: Removing suid"
 for p in /bin/fusermount /bin/mount /bin/ping /bin/ping6 /bin/su /bin/umount /usr/bin/bsd-write /usr/bin/chage /usr/bin/chfn /usr/bin/chsh /usr/bin/mlocate /usr/bin/mtr /usr/bin/newgrp /usr/bin/pkexec /usr/bin/traceroute6.iputils /usr/bin/wall /usr/sbin/pppd;
-do 
+do
 	oct=$(tat -c "%a" $p |sed 's/^4/0/')
 	ug=$(stat -c "%U %G" $p)
 	dpkg-statoverride --remove $p 2> /dev/null
