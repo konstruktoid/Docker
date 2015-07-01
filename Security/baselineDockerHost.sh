33578b217e75e4d864ee9ad6c1d47413873371be
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2015-07-01 21:18:23 +0200

    silent curl
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- Security/baselineDockerHost.sh
+++ Security/baselineDockerHost.sh
@@ -3,12 +3,12 @@
 FW_CONF="https://raw.githubusercontent.com/konstruktoid/ubuntu-conf/master/net/firewall.conf"
 FW_POLICY="https://raw.githubusercontent.com/konstruktoid/Docker/master/Security/firewall.docker"
 NTP="https://raw.githubusercontent.com/konstruktoid/ubuntu-conf/master/net/ntpconf.sh"
-ADMINIP=`grep "Accepted publickey" /var/log/auth.log | awk '{print $11}' | sort | uniq`
+ADMINIP=$(grep "Accepted publickey" /var/log/auth.log | awk '{print $11}' | sed -e 's/[^0-9.a-z]*//g' -e '/^$/d' | sort | uniq)
 
 echo ":: Setting up a firewall"
-curl $FW_CONF > /etc/init/firewall.conf
-curl $FW_POLICY > /etc/init.d/firewall
-update-rc.d firewall defaults 
+curl -s $FW_CONF > /etc/init/firewall.conf
+curl -s $FW_POLICY > /etc/init.d/firewall
+update-rc.d firewall defaults
 sed -i "s/ADMIN=.*/ADMIN=\"$ADMINIP\"/" /etc/init.d/firewall
 chmod u+x /etc/init.d/firewall
 bash /etc/init.d/firewall
@@ -35,29 +35,28 @@ done
 echo ":: Removing suid"
 for p in /bin/fusermount /bin/mount /bin/ping /bin/ping6 /bin/su /bin/umount /usr/bin/bsd-write /usr/bin/chage /usr/bin/chfn /usr/bin/chsh /usr/bin/mlocate /usr/bin/mtr /usr/bin/newgrp /usr/bin/pkexec /usr/bin/traceroute6.iputils /usr/bin/wall /usr/sbin/pppd;
 do 
-	oct=`stat -c "%a" $p |sed 's/^4/0/'`
-	ug=`stat -c "%U %G" $p`
+	oct=$(tat -c "%a" $p |sed 's/^4/0/')
+	ug=$(stat -c "%U %G" $p)
 	dpkg-statoverride --remove $p 2> /dev/null
-	dpkg-statoverride --add $ug $oct $p 2> /dev/null
+	dpkg-statoverride --add "$ug" "$oct" $p 2> /dev/null
 	chmod -s $p
 done
 
-for SHELL in `cat /etc/shells`; do
-	if [ -x $SHELL ]; then
-		chmod -s $SHELL
+for SHELL in $(cat /etc/shells); do
+	if [ -x "$SHELL" ]; then
+		chmod -s "$SHELL"
 	fi
 done
 
 echo ":: Changing default Docker options"
 sed -i 's/DOCKER_OPTS="/DOCKER_OPTS="--icc=false /' /etc/default/docker
-sed -i "s/DOCKER_OPTS='/DOCKER_OPTS='--icc=false /" /etc/default/docker
 service docker restart
 
 echo ":: Setting up NTP"
-NTPTEMP="`mktemp`"
-curl $NTP > $NTPTEMP
-sed -i 's/APPLY="no"/APPLY="YES"/' $NTPTEMP
-bash $NTPTEMP
+NTPTEMP="$(mktemp)"
+curl -s $NTP > "$NTPTEMP"
+sed -i 's/APPLY="no"/APPLY="YES"/' "$NTPTEMP"
+bash "$NTPTEMP"
 
 echo ":: Cleaning"
 apt-get -y clean
