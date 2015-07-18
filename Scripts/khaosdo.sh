8f61da7f0685f37abc5e875e8eae70be5bc483c0
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2015-07-19 01:17:12 +0200

    khaosdo.sh - push to failure
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

---
+++ Scripts/khaosdo.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# push the Docker environments to failure
+
+numContainers=$(docker ps -q | wc -l)
+
+if [ "$numContainers" -eq 0 ]; then
+  echo "No containers running. Shutting down."
+  exit 1
+fi
+
+randShuf=$(shuf -i 1-5 -n 1)
+shufContainer=$(docker ps -q | shuf -n 1 | awk '{print $1}')
+shufContainerCmd="$(docker help | awk '{print $1}' | egrep 'pause|restart|stop' |\
+  shuf -n 1)"
+containerImage=$(docker inspect -f '{{.Config.Image}}' "$shufContainer")
+containerName=$(docker inspect -f '{{.Name}}' "$shufContainer")
+
+containerCMD() {
+  containerInfo="$shufContainer ($containerName): $containerImage"
+  echo "executing $shufContainerCmd on container $containerInfo"
+  docker "$shufContainerCmd" "$shufContainer"
+}
+
+serverCMD() {
+  echo "restarting the Docker daemon"
+  systemctl restart docker.service
+}
+
+if [ "$randShuf" -le 3 ]; then
+  containerCMD
+  else
+  serverCMD
+fi
