fdd8903b2353904e8384748a3a0752af167fe0f8
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2015-11-05 18:43:06 +0100

    reduce the script, adapt to new zip.
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- Machine/createMachine.sh
+++ Machine/createMachine.sh
@@ -1,57 +1,45 @@
-#!/bin/bash
+#!/bin/sh
 # https://github.com/docker/machine
 # https://www.digitalocean.com/
 
-ACCESSTOKEN=""
-LOCATION="ams2"
+INPUT="$1"
+ACCESSTOKEN="$2"
+LOCATION="$3"
 
 MACHINE="/usr/local/bin/docker-machine"
-MACHINERELEASE="https://github.com/docker/machine/releases/download/v0.4.1/docker-machine_linux-amd64"
+MACHINERELEASE="https://github.com/docker/machine/releases/download/v0.5.0/docker-machine_linux-amd64.zip"
 HARDENING="https://raw.githubusercontent.com/konstruktoid/Docker/master/Security/baselineDockerHost.sh"
 
-INPUT="$@"
 
-if ! test -d "$TMP";
-  then
-    TMP=/tmp
+if ! test -d "$TMP"; then
+   TMP=/tmp
 fi
 
-if [ "$INPUT" == "upgrade" ];
-  then
-    cd $TMP && wget -nv $MACHINERELEASE
-    sudo mv $TMP/docker-machine_linux-amd64 /usr/local/bin/
-    sudo chown root:root /usr/local/bin/docker-machine_linux-amd64
-    sudo chmod 0755 /usr/local/bin/docker-machine_linux-amd64
-    sudo ln -vfs /usr/local/bin/docker-machine_linux-amd64 "$MACHINE"
-    docker-machine -v
-    exit
+if [ "$INPUT" = "install" ]; then
+   cd $TMP &&
+   curl -L $MACHINERELEASE > machine.zip && \
+   unzip machine.zip && \
+   rm machine.zip && \
+   sudo mv -f docker-machine* /usr/local/bin
+   sudo chown root:root /usr/local/bin/docker-machine*
+   sudo chmod 0755 /usr/local/bin/docker-machine*
+   /usr/local/bin/docker-machine -v
+   exit
 fi
 
-if [ -z $ACCESSTOKEN ];
-  then
-    echo "ACCESSTOKEN required"
-    exit
+if [ -z "$ACCESSTOKEN" ]; then
+   echo "ACCESSTOKEN required"
+   exit
 fi
 
-if ! test -x "$MACHINE";
-  then
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9 
-    sudo apt-get update && sudo apt-get -y upgrade
-    sudo apt-get install lxc-docker
-    sudo addgroup "$(whoami)" docker
-    cd $TMP && wget -nv $MACHINERELEASE
-    sudo mv $TMP/docker-machine_linux-amd64 /usr/local/bin/
-    sudo chown root:root /usr/local/bin/docker-machine_linux-amd64
-    sudo chmod 0755 /usr/local/bin/docker-machine_linux-amd64
-    sudo ln -vfs /usr/local/bin/docker-machine_linux-amd64 "$MACHINE"
-    docker-machine -v
+if ! test -x "$MACHINE"; then
+  echo "docker-machine doesn't seem to be installed."
 fi
 
-if [ -z "$INPUT" ];
-  then
-    echo "Machine name required"
-    exit
+if [ -z "$INPUT" ]; then
+   echo "Machine name required"
+   exit 1
   else
-    docker-machine create --driver digitalocean --digitalocean-access-token $ACCESSTOKEN --digitalocean-region "$LOCATION" "$@"
-    docker-machine ssh "$@" "wget -O /tmp/baselineDockerHost.sh $HARDENING; /bin/bash /tmp/baselineDockerHost.sh"
+   docker-machine create --driver digitalocean --digitalocean-access-token "$ACCESSTOKEN" --digitalocean-region "$LOCATION" "$INPUT"
+   docker-machine ssh "$INPUT" "wget -O /tmp/baselineDockerHost.sh $HARDENING; /bin/bash /tmp/baselineDockerHost.sh"
 fi
