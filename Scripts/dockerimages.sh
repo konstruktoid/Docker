a260ef66421c8423ba2526683eb516cc185eea1c
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2016-05-15 20:31:32 +0200

    order images
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- Scripts/dockerimages.sh
+++ Scripts/dockerimages.sh
@@ -1,43 +1,47 @@
 #!/bin/sh
 
 IMAGES="
-  alpine:latest
-  alpine:3.2
-  alpine:3.1
-  alpine:2.7
   alpine:2.6
+  alpine:2.7
+  alpine:3.1
+  alpine:3.2
+  alpine:latest
   busybox:latest
-  centos:7
-  centos:6.6
-  centos:6
   centos:5
+  centos:6
+  centos:6.6
+  centos:7
   centos:latest
-  debian:latest
-  debian:8
-  debian:7
   debian:6
-  fedora:22
-  fedora:21
+  debian:7
+  debian:8
+  debian:latest
   fedora:20
+  fedora:21
+  fedora:22
   fedora:latest
   konstruktoid/alpine:latest
   konstruktoid/debian:jessie
+  konstruktoid/debian:latest
   konstruktoid/debian:wheezy
+  konstruktoid/ubuntu:latest
   konstruktoid/ubuntu:trusty
   konstruktoid/ubuntu:wily
   konstruktoid/ubuntu:xenial
+  konstruktoid/ubuntu:yakkety
   opensuse:13.1
   opensuse:13.2
   opensuse:latest
-  oraclelinux:7
-  oraclelinux:6
   oraclelinux:5
+  oraclelinux:6
+  oraclelinux:7
   oraclelinux:latest
-  ubuntu:latest
-  ubuntu:16.04
-  ubuntu:15.10
-  ubuntu:14.10
   ubuntu:14.04
+  ubuntu:14.10
+  ubuntu:15.10
+  ubuntu:16.04
+  ubuntu:16.10
+  ubuntu:latest
 "
 
 date > docker_images_result
