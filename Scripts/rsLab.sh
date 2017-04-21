29eb25b9d55c7225b517c61672d1658cf99d0940
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2017-04-21 16:10:30 +0200

    update mongodb replica test
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- Scripts/rsLab.sh
+++ Scripts/rsLab.sh
@@ -8,6 +8,12 @@ NUMSERVERS=5
 
 INCR=0
 
+    #"$SERVERNAME.$NETNAME" \
+
+if ! docker network ls | grep "$NETNAME"; then
+  docker network create --attachable --internal "$NETNAME"
+fi
+
 while [ $INCR -lt $NUMSERVERS ]; do
   INCR=$((INCR + 1))
   SERVERNAME="$CNAME$INCR"
@@ -16,12 +22,26 @@ while [ $INCR -lt $NUMSERVERS ]; do
     --cap-drop=all --cap-add=setgid --cap-add=setuid \
     --name "$SERVERNAME" \
     --hostname "$SERVERNAME" \
-    --publish-service="$SERVERNAME.$NETNAME" \
+    --network "$NETNAME" \
     --detach \
     "$CIMAGE" \
     --replSet "$RSNAME" \
     --sslMode requireSSL --sslPEMKeyFile /etc/ssl/mongodb.pem
 done
 
+echo 'Creating replica set: '
+sleep 5
+docker exec -ti "$SERVERNAME" mongo --ssl --sslAllowInvalidCertificates --eval 'printjson(rs.initiate())'
+
+echo 'Adding nodes: '
+for n in $(docker inspect --format '{{ .Name }}' $(docker ps -q) | grep mongo | grep -v 'mongo5' | tr -d '/'); do
+  docker exec -ti "$SERVERNAME" mongo --ssl --sslAllowInvalidCertificates --eval "rs.add(\"$n\")"
+done
+
+echo 'Replica set status: '
+docker exec -ti "$SERVERNAME" mongo --ssl --sslAllowInvalidCertificates --eval 'printjson(rs.status())'
+
 echo
+echo 'Cheat sheet:'
 echo "docker exec -ti $SERVERNAME mongo --ssl --sslAllowInvalidCertificates"
+echo 'labRS:PRIMARY> rs.add("mongo1:27017")'
